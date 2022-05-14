5a92478ef85173a8d62371e2ac3f3ca25f84b5be
Dikshit <singhdikshit69@gmail.com>
2022-05-14 16:01:51 +0530

    adding props + images

---
+++ src/components/Text.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Text() {
+  return (
+    <>
+    <style>{`
+    .paragraph{
+        position: absolute;
+width: 233px;
+height: 52px;
+left: 696px;
+top: 2732px;
+
+font-family: 'Open Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 30px;
+line-height: 181.69%;
+/* or 55px */
+
+letter-spacing: 0.005em;
+
+color: #5B6062;
+    }
+    `}</style>
+        <p className='paragraph'>Why choose us</p>
+    </>
+  )
+}
+
+export default Text
